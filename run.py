a482b58aad5b00e2e3784de0d35f630557ba5b3a
Nikita Makarov <nikita20021804@gmail.com>
2025-03-25 09:29:58 +0200

    finally

--- run.py
+++ run.py
@@ -2,9 +2,3 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
-
-
-# testing github
-# testing again
-# I hate it
-# testing github app
